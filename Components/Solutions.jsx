29a804cd2dfd83447a16e55efb995c062bb72ad2
GGTuran <tayyabturan67@gmail.com>
2024-03-06 23:02:56 +0600

    push

--- Components/Solutions.jsx
+++ Components/Solutions.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Solutions = () => {
     return (
         <div>
-            
+            <h2></h2>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
         </div>
     );
 };
